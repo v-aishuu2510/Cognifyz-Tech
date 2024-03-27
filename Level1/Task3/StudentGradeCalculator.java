2ee3d0248eb67b8fd826db0fcf9c586a4bf1e075
Vaishali Raut <142431370+v-aishuu2510@users.noreply.github.com>
2024-03-27 20:08:46 +0530

    Update StudentGradeCalculator.java

--- Level1/Task3/StudentGradeCalculator.java
+++ Level1/Task3/StudentGradeCalculator.java
@@ -1,5 +1,6 @@
 package level1;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 //Task3
@@ -14,7 +15,17 @@ public class StudentGradeCalculator
 
 		
 		System.out.print("Enter the number of grades: ");
-		int numGrades= sc.nextInt();
+		int numGrades;
+		try
+		{
+			numGrades= sc.nextInt();
+		}
+		catch(InputMismatchException e)
+		{
+			System.out.println("Invalid input ! Enter the number only: ");
+			sc.next();
+			 numGrades= sc.nextInt();
+		}
 		System.out.println();
 		
 		int greads[]=new int[numGrades];
@@ -22,16 +33,31 @@ public class StudentGradeCalculator
 		for (int i=0;i<numGrades;i++)
 		{
 			
-			System.out.print("Enter grade "+ (i+1)+": ");
-			int gread=sc.nextInt();
-			
-			while(gread <0 || gread>100)
+			int gread;
+			boolean validInput=false;
+			while(!validInput)
 			{
-				System.out.println("Grade must be between 0 and 100.");
-				System.out.println("Enter grade "+(i+1)+": ");
-				gread=sc.nextInt();
+				try
+				{
+					System.out.print("Enter grade "+ (i+1)+": ");
+					gread=sc.nextInt();
+
+					if(gread >=0 && gread <=100)
+					{
+						greads[i]=gread;
+						validInput=true;
+					}
+					else
+					{
+						System.out.println("Grade must be between 0 and 100.");
+					}
+				}
+				catch(Exception e)
+				{
+					System.out.println("Please enter a valid numeric grad: ");
+					sc.next();
+				}
 			}
-			greads[i]=gread;
 		}
 		
 		double sum=0;
@@ -41,7 +67,8 @@ public class StudentGradeCalculator
 		}
 		
 		double averageOfGrade=sum/numGrades;
-		System.out.println("Student Name: "+name);
+		System.out.println("\nStudent Name: "+name);
 		System.out.println("The average grade is: "+averageOfGrade);
 	}
 }
+
